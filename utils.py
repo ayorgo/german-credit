76429c0759ddcae065bb697cfdba5cbb5e0b9ba5
ayorgo <george.adzhygai@gmail.com>
2019-09-15 13:52:12 +0100

    Refactor the visualization machinery

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import scipy.stats as st
-import requests
 from IPython.core.display import HTML
 import matplotlib.pyplot as plt
 from sklearn.utils import resample
@@ -22,8 +21,8 @@ class AggregatedClassifier:
 
     def predict_proba(self, X):
         return self.__aggregator([self.__predict_single(estimator, X)
-                                 for estimator
-                                 in self.estimators],
+                                  for estimator
+                                  in self.estimators],
                                  **self.kwargs)
 
 
@@ -85,7 +84,8 @@ def train(x, y, estimator, params, iter_no):
                                  cv=StratifiedKFold(n_splits=5),
                                  scoring='roc_auc',
                                  refit='roc_auc',
-                                 n_jobs=-1)
+                                 n_jobs=-1,
+                                 iid=True)
         grid_srch.fit(x_train, y_train)
 
         y_pred_proba = grid_srch.predict_proba(x_test)[:, 1]
@@ -95,7 +95,7 @@ def train(x, y, estimator, params, iter_no):
                'y_pred_proba': y_pred_proba}
 
 
-def calculate_metrics(models, threshold):
+def collect_metrics(models, threshold):
     """ Calculate performance metrics
     """
 
@@ -132,42 +132,53 @@ def calculate_metrics(models, threshold):
         metric['roc_auc'] = roc_auc_score(model['y_test'],
                                           model['y_pred_proba'],
                                           average='micro')
+
         yield metric
 
 
-def visualize_metrics(metrics):
-    """ Plot/pring the metrics object
+def aggregate_metrics(metrics):
+    """ Aggregate metrics from different models
     """
 
-    tpr_mean = np.mean([m['tpr'] for m in metrics])
-    tnr_mean = np.mean([m['tnr'] for m in metrics])
-    fpr_mean = np.mean([m['fpr'] for m in metrics])
-    fnr_mean = np.mean([m['fnr'] for m in metrics])
+    metric = dict()
+    metric['tpr_mean'] = np.mean([m['tpr'] for m in metrics])
+    metric['tnr_mean'] = np.mean([m['tnr'] for m in metrics])
+    metric['fpr_mean'] = np.mean([m['fpr'] for m in metrics])
+    metric['fnr_mean'] = np.mean([m['fnr'] for m in metrics])
 
-    acc_mean = np.mean([m['acc'] for m in metrics])
-    precision_mean = np.mean([m['precision'] for m in metrics])
-    recall_mean = np.mean([m['recall'] for m in metrics])
+    metric['acc_mean'] = np.mean([m['acc'] for m in metrics])
+    metric['precision_mean'] = np.mean([m['precision'] for m in metrics])
+    metric['recall_mean'] = np.mean([m['recall'] for m in metrics])
 
-    roc_auc_mean = np.mean([m['roc_auc'] for m in metrics])
+    roc_auc = [m['roc_auc'] for m in metrics]
+    metric['roc_auc_mean'] = np.mean(roc_auc)
 
-    fpr_roc_mean = np.mean([m['fpr_roc'] for m in metrics], axis=0)
-    tpr_roc_mean = np.mean([m['tpr_roc'] for m in metrics], axis=0)
+
+    metric['fpr_roc_mean'] = np.mean([m['fpr_roc'] for m in metrics], axis=0)
+    metric['tpr_roc_mean'] = np.mean([m['tpr_roc'] for m in metrics], axis=0)
+
+    return metric
+
+
+def visualize_metrics(metric):
+    """ Visualize the metric object
+    """
+
+    roc_label = f"AUC: {metric['roc_auc_mean']:.3f}"
 
     print()
-    print(f'True Positives: {tpr_mean:.1f}%')
-    print(f'True Negatives: {tnr_mean:.1f}%')
-    print(f'False Positives: {fpr_mean:.1f}%')
-    print(f'False Negatives: {fnr_mean:.1f}%')
+    print(f"True Positives: {metric['tpr_mean']:.1f}%")
+    print(f"True Negatives: {metric['tnr_mean']:.1f}%")
+    print(f"False Positives: {metric['fpr_mean']:.1f}%")
+    print(f"False Negatives: {metric['fnr_mean']:.1f}%")
     print()
-    print(f'Accuracy: {acc_mean:.1f}')
-    print(f'Precision: {precision_mean:.1f}')
-    print(f'Recall: {recall_mean:.1f}')
-    print(f'Area under the curve: {roc_auc_mean:.3f}')
+    print(f"Accuracy: {metric['acc_mean']:.1f}")
+    print(f"Precision: {metric['precision_mean']:.1f}")
+    print(f"Recall: {metric['recall_mean']:.1f}")
+    print(roc_label)
 
-    area = auc(fpr_roc_mean, tpr_roc_mean)
-    plt.plot(fpr_roc_mean,
-             tpr_roc_mean,
-             label=f'ROC curve (area = {area:.3f})')
+    plt.plot(metric['fpr_roc_mean'], metric['tpr_roc_mean'], label=roc_label)
+
     plt.plot([0, 1], [0, 1], 'k--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.0])
@@ -175,9 +186,11 @@ def visualize_metrics(metrics):
     plt.ylabel('True Positive Rate')
     plt.title('Receiver Operating Characteristic')
     plt.legend(loc='lower right')
-    plt.fill_between(fpr_roc_mean, tpr_roc_mean, color='silver')
+    plt.fill_between(metric['fpr_roc_mean'],
+                     metric['tpr_roc_mean'],
+                     color='silver')
 
-    plt.plot(fpr_mean/(fpr_mean + tnr_mean),
-             tpr_mean/(tpr_mean + fnr_mean),
+    plt.plot(metric['fpr_mean']/(metric['fpr_mean'] + metric['tnr_mean']),
+             metric['tpr_mean']/(metric['tpr_mean'] + metric['fnr_mean']),
              marker='o',
              color='black')
